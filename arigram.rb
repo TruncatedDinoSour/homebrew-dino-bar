41ca1d0422ba7d9a5881cccffde1b26d0f3dee2b
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2021-11-28 17:53:07 +0800

    Create arigram.rb

---
+++ arigram.rb
@@ -0,0 +1,31 @@
+class Arigram < Formula
+  include Language::Python::Virtualenv
+
+  desc "Terminal telegram client"
+  homepage "https://github.com/TruncatedDinosour/arigram"
+  url "https://github.com/TruncatedDinosour/arigram/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "3e8d7ff632f86c478952bd8935e67264b3984df63ca1f4c9dc9caaaf37fa0de7"
+  license "Unlicense"
+
+  depends_on "python@3.9"
+  depends_on "tdlib"
+
+  resource "python-telegram-patched" do
+    url "https://github.com/paul-nameless/python-telegram/archive/refs/tags/0.14.0-patched.tar.gz"
+    sha256 "7f3af4cb722f37bd65b0a49da448b7d26afa7f2de761aed4828018155e2e78b4"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    require "pty"
+
+    PTY.spawn(bin/"tg") do |r, w, _pid|
+      assert_match "Enter your phone", r.gets
+      w.close
+      r.close
+    end
+  end
+end
